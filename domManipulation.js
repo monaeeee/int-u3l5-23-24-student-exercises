61e5b55a0cf0d839bd28aa1979aa85087375b163
monaeeee <147624231+monaeeee@users.noreply.github.com>
2024-01-31 14:51:14 +0000

    worked

--- domManipulation.js
+++ domManipulation.js
@@ -9,15 +9,15 @@ let row1 = document.querySelector(".row-1");
 // CHALLENGE #1 - make the rows match
 
 // 1. Set the styling of `example1` to a background color of black.
-
+example1.style.backgroundColor="black";
 // 2. Set the styling of `pink` to a background color of pink.
-
+pink.style.backgroundColor="pink";
 // 3. Set the styling of `green` to a background color of green.
-
+green.style.backgroundColor="green";
 // 4. Set the styling of `blue` to a background color of blue.
-
+blue.style.backgroundColor="blue";
 // 5. Set the styling of `lightGreen` to a background color of lightGreen.
-
+lightGreen.style.backgroundColor="lightGreen";
 // 6. Set the styling of `row1` to have a font color of white.
 
 
@@ -29,7 +29,8 @@ let imageButton = document.querySelector(".image-button");
                                    
 // 7. Add an event listener to `imageButton`
 // - The event type should be "click" or "dblclick"
-
+image.style.width="300px";
+image.style.border="soild 5px #42068c";
 // Include the next tasks in the body of the function.
 
 // 8. The image is wayyyy too big and needs a border.
